ff390ee1f59fc8a36a8eef105f56204e5ba8a568
José Francisco Hernández Balestrini <kikohernandez92@me.com>
2019-03-28 13:14:44 -0500

    First docker image test

--- Preparation.sh
+++ Preparation.sh
@@ -14,6 +14,10 @@ apt install -y php7.2-common php7.2-mysql php7.2-cgi
 apt install -y php7.2-curl php7.2-zip php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-xsl
 apt install -y php7.2-dev php7.2-bz2 php7.2-intl php7.2-json php7.2-opcache php7.2-readline 
 apt install -y php7.2-imap php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-pgsql
+apt install -y composer php7.2-bcmath
+composer global require laravel/installer
+cd adminpanel && composer require tcg/voyager
+cp envconfig .env
 
 # Enable PHP 7.2 in Apache
 a2enmod php7.2
