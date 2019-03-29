de3563ac25ddd272631612a971d4a98badf7e294
José Francisco Hernández Balestrini <kikohernandez92@me.com>
2019-03-29 09:41:25 -0500

    Fully dockerized with Backpack

--- Launch.sh
+++ Launch.sh
@@ -4,4 +4,4 @@ docker network create --driver=overlay --attachable swarmnet
 docker stack deploy -c db.yml db
 #docker build -t kikohernandez92/laravel .
 #docker push kikohernandez92/laravel
-#docker stack deploy -c stack.yml prod
+docker stack deploy -c stack.yml prod
