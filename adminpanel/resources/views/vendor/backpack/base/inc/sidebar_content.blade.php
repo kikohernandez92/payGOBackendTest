c9e863808e53531da3cb0f5014540308bc783433
José Francisco Hernández Balestrini <kikohernandez92@me.com>
2019-04-02 16:22:38 -0500

    Bugfix

--- adminpanel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ adminpanel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,2 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-
-
-<li><a href='{{ route('ieview') }}'><i class='fa fa-users'></i> <span>Cargar Archivo</span></a></li>
-<li><a href='{{ backpack_url('empleado') }}'><i class='fa fa-users'></i> <span>Empleados</span></a></li>
